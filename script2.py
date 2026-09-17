80e0fdf01de6ddb78b42fe0c65af35ee2ef3eb7e
prashanth <projects2488@gmail.com>
2026-09-17 10:16:16 +0530

    new file

---
+++ script2.py
@@ -0,0 +1,17 @@
+# Simple arithmetic operations in Python
+
+# Input values
+num1 = 10
+num2 = 5
+
+# Arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
